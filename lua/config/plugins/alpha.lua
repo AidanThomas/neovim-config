56140522de5b82f6dc6e38773def8fe6cf76108c
Aidan Thomas <aidant@agylia.com>
2023-12-23 19:12:42 +0000

    Update dashboard bindings

--- lua/config/plugins/alpha.lua
+++ lua/config/plugins/alpha.lua
@@ -82,11 +82,11 @@ local options = {
 	buttons = {
 		type = "group",
 		val = {
-			button("SPC f f", "  Find File", ":Telescope find_files<CR>"),
-			button("SPC f o", "󰈚  Recent File", ":Telescope oldfiles<CR>"),
-			button("SPC f w", "󰈭  Find Word", ":Telescope live_grep<CR>"),
-			button("SPC b m", "  Bookmarks", ":Telescope marks<CR>"),
-			button("SPC e s", "  Settings", ":Telescope find_files cwd=~/.config/nvim<CR>"),
+			button("e", "  Create File", ":Telescope file_browser<CR>"),
+			button("f f", "  Find File", ":Telescope find_files<CR>"),
+			button("f w", "󰈭  Find Word", ":Telescope live_grep<CR>"),
+			button("b m", "  Bookmarks", ":Telescope marks<CR>"),
+			button("e s", "  Settings", ":Telescope find_files cwd=~/.config/nvim<CR>"),
 		},
 		opts = {
 			spacing = 1,

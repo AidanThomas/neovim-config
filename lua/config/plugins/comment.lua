1f1ea7d667c01a2b018a740f80485a32c73af25b
Aidan Thomas <aidant@agylia.com>
2024-03-01 00:16:45 +0000

    Clean up comment config

--- lua/config/plugins/comment.lua
+++ lua/config/plugins/comment.lua
@@ -1,58 +1,37 @@
 require("Comment").setup({
-	-- Add a space between comment and line
-	padding = true,
-	-- Whether the cursor should stay at its position
-	sticky = true,
-	-- LHS of toggles mappings in NORMAL mode
-	toggle = {
-		line = 'gcc',
-		block = 'gbc',
-	},
-	-- LHS of operator-pending mappings in NORMAL and VISUAL mode
-	opleader = {
-		line = 'gc',
-		block = 'gbc'
-	},
-	-- LHS of extra mappings
-	extra = {
-		above = 'gcO', -- Add comment on line above
-		below = 'gco', -- Add comment on line below
-		eol = 'gca' -- Add comment at the end of line
-	},
+    -- Add a space between comment and line
+    padding = true,
+    -- Whether the cursor should stay at its position
+    sticky = true,
+    -- LHS of toggles mappings in NORMAL mode
+    toggler = {
+        line = 'gcc',
+        block = 'gbc',
+    },
+    -- LHS of operator-pending mappings in NORMAL and VISUAL mode
+    opleader = {
+        line = 'gc',
+        block = 'gbc'
+    },
+    -- LHS of extra mappings
+    extra = {
+        above = 'gcO', -- Add comment on line above
+        below = 'gco', -- Add comment on line below
+        eol = 'gca'    -- Add comment at the end of line
+    },
 
-	-- Enable keybindings
-	mappings = {
-		basic = true,
-		extra = true,
-	},
+    -- Enable keybindings
+    mappings = {
+        basic = true,
+        extra = true,
+    },
 
-	--Function to call before (un)comment
-	pre_hook = nil,
-	--Function to call after (un)comment
-	post_hook = nil,
+    ignore = '^$',
 
-	textobjects = {
-		select = {
-			enable = true,
-
-			-- Automatically jump forward to textobj, simlilar to targets.vim
-			lookahead = true,
-
-			keymaps = {
-				["af"] = { query = "@function.outer", desc = "Select outer part of a function region" },
-				["if"] = { query = "@function.inner", desc = "Select inner part of a function region" },
-				["ac"] = { query = "@class.outer", desc = "Select out part of a class region" },
-				["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-				["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-			},
-
-			selection_modes = {
-				["@parameter.outer"] = "v", -- charwise
-				["@function.outer"] = "V", -- linewise
-				["@class.outer"] = "<c-v>", -- blockwise
-			},
-
-			include_surrounding_whitespace = true,
-		},
-	},
+    --Function to call before (un)comment
+    ---@diagnostic disable-next-line: assign-type-mismatch
+    pre_hook = nil,
+    --Function to call after (un)comment
+    ---@diagnostic disable-next-line: assign-type-mismatch
+    post_hook = nil,
 })
